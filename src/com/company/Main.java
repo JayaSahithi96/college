6c71cf224522a8eae8ddbf7d51713c73ef1d2e84
sahithi31 <jsahithi90@gmail.com>
2021-01-17 19:14:05 -0800

    mobile learning app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Studnet s1= new Studnet();
+        Studnet s1= new Studnet(121,"Sahithi","Aravapalli");
         s1.getFirstName();
     }
 }
--- src/com/company/Studnet.java
+++ src/com/company/Studnet.java
@@ -2,12 +2,17 @@ package com.company;
 
 public class Studnet extends Person{
     private long studentId;
+    private String firstName;
+    private String lastName;
 
     public Studnet(){}
-    public Studnet(long studentId){
+    public Studnet(long studentId, String firstName,String lastName){
+        super();
         this.studentId=studentId;
-
+        this.firstName=firstName;
     }
+
+
     public long getStudentId(){
 
         return studentId;
