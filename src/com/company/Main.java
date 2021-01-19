5ba534e0c69dbb34fa9cd3782e5e61b3ee852ea0
sahithi31 <jsahithi90@gmail.com>
2021-01-18 17:27:16 -0800

    mobile learning app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Map<Integer,Student> map= new HashMap<>();
+        Map<Long,Student> map= new HashMap<>();
 
         Student s1= new Student(121,"Jaya Sahithi","Aravapalli","jsahithi@gmail.com");
         Student s2= new Student(122,"Akhil","Madineni","akhil@gmail.com");
         Student s3= new Student(123,"Praveen","hande","praveen@gmail.com");
 
        // System.out.println("student details are:"+s1.getStudentId()+ "\n" +s1.getFirstName()+ "\n" +s1.getEmailId() );
-        map.put
-        s1.printDetails();
+        map.put(s1.getStudentId(),s1);
+        map.put(s2.getStudentId(),s2);
+        map.put(s3.getStudentId(),s3);
+        for (Map.Entry<Long,Student> entry : map.entrySet()){
+            System.out.println(entry.getKey());
+            entry.getValue().printDetails();
+        }
+
 
     }
 }
