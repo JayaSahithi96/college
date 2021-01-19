9a58460982036806c876eeca250b80bf336f5d8a
sahithi31 <jsahithi90@gmail.com>
2021-01-18 17:12:28 -0800

    mobile learning app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,22 @@
 package com.company;
+import com.company.Student;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //Person p1= new Person("Jaya","Aravapalli","tfytftjy@gamail.com","916575587","home","f",24);
-        Student s1= new Student(121);
-        System.out.println("student details are:"+s1.getFirstName()+ "\n" +s1.getEmailId() );
+        Map<Integer,Student> map= new HashMap<>();
+
+        Student s1= new Student(121,"Jaya Sahithi","Aravapalli","jsahithi@gmail.com");
+        Student s2= new Student(122,"Akhil","Madineni","akhil@gmail.com");
+        Student s3= new Student(123,"Praveen","hande","praveen@gmail.com");
+
+       // System.out.println("student details are:"+s1.getStudentId()+ "\n" +s1.getFirstName()+ "\n" +s1.getEmailId() );
+        map.put
+        s1.printDetails();
 
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,19 +1,38 @@
 package com.company;
 
-public class Student extends Person{
-    private long studentId;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Student(){}
-    public Student(long studentId){
-       super("Jaya","Aravapalli","tfytftjy@gamail.com","916575587","home","f",24);
+public class Student extends Person{
+
+    private long studentId;
+    String firstName;
+    String lastName;
+    String emailId;
+    String phoneNumber,address;
+    String gender;
+    int age;
+
+    public Student(){
+        super();
+    }
+    public Student(long studentId,String firstName,String lastName,String emailId){
+       super(firstName,lastName,emailId,"9163456784","home","f",24);
+
         this.studentId=studentId;
-        this.firstName=firstName;
+      //  this.firstName=firstName;
+        //this.lastName=lastName;
+        //this.emailId=emailId;
     }
 
 
     public long getStudentId(){
 
         return studentId;
+    }
+    public void printDetails(){
+        //System.out.println("Id is:"+studentId);
+        System.out.println("student details are:"+ studentId + "\n" +getFirstName()+ "\n" +getLastName()+ "\n" +getEmailId() );
     }
 
 }
